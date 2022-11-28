55a2a7c632aed3f19d6e013668f50f379eff948f
Robot-9 <mr.stepanyan.2006@mail.ru>
2022-11-28 20:17:43 +0300

    events consumer works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,9 +6,6 @@ import io.github.humbleui.jwm.Event;
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
-    public void run() {
-
-    }
 
     @Override
     public void accept(Event event) {
