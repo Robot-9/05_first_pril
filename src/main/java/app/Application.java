5a0ea078d183c72402b9c26945e46550ea16b7dd
Robot-9 <mr.stepanyan.2006@mail.ru>
2023-01-01 23:14:39 +0300

    canvas works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,6 +1,8 @@
 package app;
 
 import io.github.humbleui.jwm.*;
+import io.github.humbleui.jwm.skija.EventFrameSkija;
+import io.github.humbleui.skija.Surface;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -11,7 +13,6 @@ public class Application implements Consumer<Event> {
     public Application() {
         window = App.makeWindow();
         window.setEventListener(this);
-        window.setVisible(true);
 
         window.setTitle("Java 2D");
         window.setWindowSize(900, 900);
@@ -25,6 +26,22 @@ public class Application implements Consumer<Event> {
         String[] layerNames = new String[] {
                 "LayerGLSkija", "LayerRasterSkija"
         };
+
+        for (String layerName : layerNames) {
+            String className = "io.github.humbleui.jwm.skija." + layerName;
+            try {
+                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
+                window.setLayer(layer);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка создания слоя " + className);
+            }
+        }
+
+        if (window._layer == null)
+            throw new RuntimeException("Нет доступных слоёв для создания");
+
+        window.setVisible(true);
     }
 
     @Override
@@ -33,6 +50,9 @@ public class Application implements Consumer<Event> {
             App.terminate();
         } else if (e instanceof EventWindowCloseRequest) {
             window.close();
+        } else if (e instanceof EventFrameSkija ee) {
+            Surface s = ee.getSurface();
+            s.getCanvas().clear(0xFF264653);
         }
     }
 }
