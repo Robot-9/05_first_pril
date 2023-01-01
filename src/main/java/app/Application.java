c781fd650336cc8a69dad55a0edda7b9edb900a9
Robot-9 <mr.stepanyan.2006@mail.ru>
2023-01-01 23:22:49 +0300

    add setColor()

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -7,6 +7,8 @@ import io.github.humbleui.skija.Surface;
 import java.io.File;
 import java.util.function.Consumer;
 
+import static app.Colors.APP_BACKGROUND_COLOR;
+
 public class Application implements Consumer<Event> {
     private final Window window;
 
@@ -52,7 +54,7 @@ public class Application implements Consumer<Event> {
             window.close();
         } else if (e instanceof EventFrameSkija ee) {
             Surface s = ee.getSurface();
-            s.getCanvas().clear(0xFF264653);
+            s.getCanvas().clear(APP_BACKGROUND_COLOR);
         }
     }
 }
---
+++ src/main/java/misc/Misc.java
@@ -0,0 +1,11 @@
+package misc;
+
+public class Misc {
+    public static int getColor(int a, int r, int g, int b) {
+        return ((a * 256 + r) * 256 + g) * 256 + b;
+    }
+
+    private Misc() {
+        throw new AssertionError("Вызов этого конструктора запрещён");
+    }
+}
